9e940ce6f70c22f5eb5b0626ce6f0249c0331b88
jcwang111 <23427258+jcwang111@users.noreply.github.com>
2021-02-25 17:35:29 -0800

    reorganization

---
+++ particle_swarm.py
@@ -0,0 +1,52 @@
+import pyswarms as ps
+from math import prod
+
+def optim_particle_swarm(cost, param_shape, num_particles=12, iters=100, init_weight=None):
+    """Optimizes the parameters with PySwarms particle swarm optimization
+        Args:
+            cost: cost function, takes in a numpy array and outputs a scalar
+            param_shape: shape of the argument of cost
+            iters: number of iterations
+            init_weights: optional, initial position for the optimization
+        Returns:
+            params: parameters with lowest cost found by optimizer
+    """
+    size = prod(param_shape)
+
+    def f(x):
+        """Function to pass into pyswarms"""
+        cost_list = [cost(x[i].reshape(param_shape)) for i in range(num_particles)]
+        return np.array(cost_list)
+
+    if init_weight != None:
+        init_weight = np.broadcast_to(init_weight.reshape(-1), (num_particles, size))
+
+    options = {'c1': 0.5, 'c2': 0.5, 'w':0.5}
+    optimizer = ps.single.GlobalBestPSO(
+                                n_particles=num_particles, dimensions=size,
+                                options=options, init_pos=init_weight)
+    final_cost, pos = optimizer.optimize(f, iters=100, n_processes=None)
+    print("Final Cost:", final_cost)
+    return pos.reshape(param_shape)
+
+if __name__ == "__main__":
+    from loss_functions import KL_Loss, KL_Loss_dict
+
+    #Generate an exact probability distribution to test on
+    exact_prob_dict = {i:1/64 for i in range(64)}
+    
+    from qcbm_train import *
+    def exact_cost_fn(weights):
+        """Loss using exact QCBM probabilities"""
+        return KL_Loss(exact_prob_dist, qcbm_probs(weights, num_wires))
+
+    def approx_cost_fn(weights):
+        """Loss using QCBM probabilities estimated from sampling"""
+        return KL_Loss_dict(exact_prob_dict, qcbm_approx_probs(weights, num_wires))
+
+    weights = initialize_weights(num_layers, num_wires)
+    print(weights.shape)
+    print(f"num_wires: {num_wires}, num_shots: {num_shots}, num_layers: {num_layers}")
+    best_weights = optim_particle_swarm(approx_cost_fn, weights.shape, iters=100, init_weight=weights)
+
+    print("Final exact cost:", KL_Loss(exact_prob_dist, qcbm_probs(weights, num_wires)))
--- qcbm_train.py
+++ qcbm_train.py
@@ -8,8 +8,7 @@ import os
 
 num_layers = 7
 num_wires = 8
-num_shots = 20_000
-#np.random.seed(0)
+num_shots = 10_000
 
 load_dotenv()
 API_KEY = os.environ.get("FLOQ_KEY")
@@ -23,6 +22,14 @@ except:
     dev = qml.device("default.qubit", wires=num_wires, shots=num_shots)
     
 #dev = qml.device("default.qubit", wires=num_wires, analytic=True, shots=num_shots)
+
+def to_num(x):
+    '''Converts array of -1 and +1 to 0 and 1, then expresses as binary number
+    Example: x = [1, -1, 1, -1, 1, 1] -> 101011 = 43'''
+
+    bitstr = ''.join(['0' if i==-1 else '1' for i in x])
+    return eval('0b' + bitstr)
+
 ######################
 
 def template_ansatz(weights, num_wires):
@@ -60,26 +67,6 @@ def qcbm_sample(weights, num_wires):
     ansatz(weights,num_wires)
     return [qml.sample(qml.PauliZ(i)) for i in range(num_wires)]
 
-######################
-
-def KL_Loss(P, Q, eps=0.1/num_shots):
-    """A loss function: Kullback-Leibler divergence AKA relative entropy between two probability distributions.
-    Args:
-        P, Q: arrays of shape (2**num_wires,), representing probability distributions
-        eps: small value, prevents divergence in log if Q = 0"""
-
-    mask = P > 0
-    return np.sum(P[mask] * np.log(P[mask] / (Q[mask]+eps)) )
-
-#######################
-
-def to_num(x):
-    '''Converts array of -1 and +1 to 0 and 1, then expresses as binary number
-    Example: x = [1, -1, 1, -1, 1, 1] -> 101011 = 43'''
-
-    bitstr = ''.join(['0' if i==-1 else '1' for i in x])
-    return eval('0b' + bitstr)
-
 def qcbm_approx_probs(weights, num_wires):
     """Approximate probabilities for the results. Alternative to qcbm_probs, which needs 2^num_qubit entries and would be
         large if we used 26~32 qubits
@@ -93,27 +80,14 @@ def qcbm_approx_probs(weights, num_wires):
     
     return prob_dict
 
-def KL_Loss_dict(p_dict, q_dict, eps=0.1/num_shots):
-    """Kullback-Leibler divergence, but using dicts as arguments instead"""
-    cost = 0.0
-    for outcome, prob in p_dict.items():
-        #print(outcome, prob, q_dict.get(outcome))
-        cost += prob * np.log(prob / (q_dict.get(outcome, eps)))
-    return cost
-
 ####################
 
-def SPSA_grad(f, theta, delta=1E-2):
+def SPSA_grad(f, theta, delta=1E-3):
     """gradient of f at theta, approximated using
         Simultaneous Perturbation Stochastic Approximation"""
 
     dt = np.random.choice([-delta,+delta], theta.shape)
     return (f(theta+dt) - f(theta - dt) ) / dt
-
-def particle_swarm_optim(f, init_params):
-    """Find global minimum using the particle swarm optimization algorithm"""
-    # Can also use Pyswarm library. This can perhaps give better results than SPSA
-    pass
 
 #########################
     
@@ -130,49 +104,40 @@ def initialize_weights(layers, num_wires):
                 a[l].append([np.random.random()*np.pi*2 for _ in range(num_wires-1-i)])
     return np.array(a)
 
-#########################
-#########################
 if __name__ == "__main__":
-    #Test the model
+    from loss_functions import KL_Loss, KL_Loss_dict
 
+    #Test the model
     weights = initialize_weights(num_layers, num_wires)
     print(f"num_wires: {num_wires}, num_shots: {num_shots}, num_layers: {num_layers}")
 
-    #For testing, generate an exact probability distribution to learn
+    #Generate an exact probability distribution to learn
     exact_prob_dist = np.random.random(2**num_wires)
     exact_prob_dist /= np.sum(exact_prob_dist)
     
-    #Convert the probability distribution into dict form
+    #Convert probability distribution into dict form
     exact_prob_dict = {outcome:exact_prob_dist[outcome] for outcome in range(2**num_wires)}
+    #Generate an exact probability distribution to test on
 
-    #Two possible cost functions
-    #########
-    #Uses exact QCBM probability
+    exact_prob_dist = np.zeros(256)
+    exact_prob_dist[0:64] = 1/64
+    exact_prob_dict = {i:1/64 for i in range(64)}
+
+    #Exact QCBM probability
     def exact_cost_fn(weights):
         return KL_Loss(exact_prob_dist, qcbm_probs(weights, num_wires))
     exact_grad_cost = qml.grad(exact_cost_fn, argnum=0)
 
-    #Uses approximate QCBM probability from samples
+    #Approximate QCBM probability, from sampling
     def approx_cost_fn(weights):
         return KL_Loss_dict(exact_prob_dict, qcbm_approx_probs(weights, num_wires))
-    #########
 
     print("Training using approximate sample probabilities")
-    for i in range(15_000):
-        weights = weights - 0.01* SPSA_grad(approx_cost_fn, weights) #cost using approx sample probabilities
-        #weights = weights - 0.01* exact_grad_cost(weights) #cost using exact sample probabilities
-        if i % 100 == 0:
-            #print("Approx Cost:", KL_Loss_dict(exact_prob_dict, qcbm_approx_probs(weights, num_wires)))
-            print("True Cost:", KL_Loss(exact_prob_dist, qcbm_probs(weights, num_wires)))
+    for i in range(1000):
+        weights = weights - 0.01* SPSA_grad(approx_cost_fn, weights) #Approximate gradient, approx. probabilities
+        #weights = weights - 0.01* exact_grad_cost(weights) #Exact gradient, exact probabilities
+        
+        if i % 10 == 0:
+            print("Exact cost:", KL_Loss(exact_prob_dist, qcbm_probs(weights, num_wires)))
 
-    #current results: analytic probabilities do better than sampled probabilities. Maybe adjust eps?
-    #SPSA is fast. will try particle swarm optimization next
-    #Still hard to train using approximate probabilities, even with 50000 shots the cost doesn't get low
-    #100_000 shots: cost gets down to 0.53, slow on Floq though
-    
-    #Even with exact, analytic gradient, lower limit to cost seems to be
-    #Perhaps changing the ansatz may help?
-
-    print(KL_Loss(exact_prob_dist, qcbm_probs(weights, num_wires)))
-    print(qcbm_sample(weights, num_wires).shape)
-    print(to_num(qcbm_sample(weights, num_wires)[:, 0]))
+    print("Final exact cost:", KL_Loss(exact_prob_dist, qcbm_probs(weights, num_wires)))
